15018b8f3de02e1b4bbc325bffe65c8432023b4b
Walid Wazini <wazini25@gmail.com>
2022-01-30 16:29:12 +0800

    usefowardRef for focussing error

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, {
+  useState, useEffect,
+  useReducer, useContext, useRef
+} from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
@@ -39,6 +42,8 @@ const Login = (props) => {
   })
 
   const authCtx = useContext(AuthContext)
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -89,13 +94,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus()
+    } else {
+      passwordInputRef.current.focus()
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id='email' label='E-mail' type='email'
           isValid={emailIsValid}
           value={emailState.value}
@@ -103,6 +115,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id='password' label='Password' type='password'
           isValid={passwordIsValid}
           value={passwordState.value}
@@ -110,7 +123,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
